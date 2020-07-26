5a2d2f1838c7a0a41638fe5fdf03b797b7185ed5
0sunzero0 <21500209@handong.edu>
2020-07-27 08:45:12 +0900

    add parameter about workers

--- topicmodel/makeWord2VecModel0.py
+++ topicmodel/makeWord2VecModel0.py
@@ -8,7 +8,7 @@ with open('/home/hyeyoung/NKDB/data/total_morphs_list.txt', 'rb') as f:
 from gensim.models import word2vec
 
 data = corpus
-model = word2vec.Word2Vec(data, size = 100, window = 5, min_count=5, sg=0) #CBOW
+model = word2vec.Word2Vec(data, size = 100, window = 5, min_count=5, workers = 5, sg=0) #CBOW
 # 100차원 벡터,
 # 출현 빈도는 5개 미만은 제외
 # 분석 방법론은 CBOW을 선택
